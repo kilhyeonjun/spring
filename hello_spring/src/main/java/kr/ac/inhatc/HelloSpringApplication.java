3f287127572b4d8ba3bb9e68e04ea13c91081acf
kboxstar@gmail.com <psg980727!>
2021-03-25 16:07:12 +0900

    db connecting (1/2)

--- hello_spring/src/main/java/kr/ac/inhatc/HelloSpringApplication.java
+++ hello_spring/src/main/java/kr/ac/inhatc/HelloSpringApplication.java
@@ -1,9 +1,11 @@
 package kr.ac.inhatc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = {"kr.ac.inhatc.mvc.mapper"})
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
